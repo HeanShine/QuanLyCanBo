ffacbb41857c789fc6f1c61ac4c766d6490e084c
Shine <113924792+HeanShine@users.noreply.github.com>
2023-06-14 15:01:04 +0700

    demo

---
+++ src/Manage/ManagerCadres.java
@@ -0,0 +1,40 @@
+package Manage;
+
+import ImplementInterface.InterfaceCares;
+import Model.Cadres;
+
+import java.util.ArrayList;
+
+public class ManagerCadres implements InterfaceCares {
+    ArrayList<Cadres> listCadres = new ArrayList<>();
+
+    @Override
+    public boolean isCadres(Integer idCheck) {
+        for (int i = 0; i < listCadres.size(); i++) {
+            if (listCadres.get(i).getId() == idCheck) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    @Override
+    public void addCadres(Cadres cadres) {
+        if (isCadres(cadres.getId())) {
+            System.out.println("ID is exist");
+        } else {
+            listCadres.add(cadres);
+        }
+    }
+
+    @Override
+    public void showCadres() {
+        if (listCadres.isEmpty()) {
+            System.out.println("List is empty");
+        }
+        for (int i = 0; i < listCadres.size(); i++) {
+            System.out.println(listCadres.get(i));
+        }
+    }
+}
---
+++ src/MenuManage/MenuManageCadres.java
@@ -0,0 +1,77 @@
+package MenuManage;
+
+import Manage.ManagerCadres;
+import Model.Engineer;
+import Model.Staff;
+import Model.Worker;
+
+import java.util.Scanner;
+
+public class MenuManageCadres {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int choice = 0;
+        ManagerCadres managerCadres = new ManagerCadres();
+
+        while (true) {
+            System.out.println("1. Add cadres");
+            System.out.println("2. Show cadres");
+            System.out.println("3. Exit");
+            System.out.println("Enter your choice: ");
+
+            do {
+                System.out.println("enter selection : ");
+                try {
+                    choice = Integer.parseInt(scanner.nextLine());
+                    break;
+                } catch (NumberFormatException e) {
+                    System.out.println("Invalid input. Please Re-enter");
+                }
+            } while (choice != 1 && choice != 2 && choice != 3);
+
+            switch (choice) {
+                case 1:
+                    System.out.println("1. Add staff");
+                    System.out.println("2. Add engineer");
+                    System.out.println("3. Add worker");
+                    System.out.println("Enter your choice: ");
+                    int choice1 = 0;
+                    do {
+                        System.out.println("enter selection : ");
+                        try {
+                            choice1 = Integer.parseInt(scanner.nextLine());
+                            break;
+                        } catch (NumberFormatException e) {
+                            System.out.println("Invalid input. Please Re-enter");
+                        }
+                    } while (choice1 != 1 && choice1 != 2 && choice1 != 3);
+                    switch (choice1) {
+                        case 1:
+
+                            Staff staff = new Staff();
+                            staff.inputStaff();
+                            managerCadres.addCadres(staff);
+                            break;
+                        case 2:
+                            Engineer engineer = new Engineer();
+                            engineer.inputEngineer();
+                            managerCadres.addCadres(engineer);
+                            break;
+                        case 3:
+                            Worker worker = new Worker();
+                            worker.inputWorker();
+                            managerCadres.addCadres(worker);
+                            break;
+                    }
+                    break;
+
+                case 2:
+                    managerCadres.showCadres();
+                    break;
+                case 3:
+                    System.exit(0);
+            }
+        }
+    }
+}
+
